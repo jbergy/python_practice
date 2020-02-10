53702b6a17bb690ba390ffb47e2d44800770169d
jbergy <bergmannjesse@gmail.com>
2020-02-10 17:50:28 -0600

    reverse string

---
+++ reverse_a_string/reverse_a_string.py
@@ -0,0 +1,7 @@
+some_string = input("Enter text to reverse. ")
+
+# index test slice and stride
+
+print("Every second index " +  some_string[::2])
+print("First four " + some_string[:4])
+print("The reversed string " + some_string[::-1]) 
